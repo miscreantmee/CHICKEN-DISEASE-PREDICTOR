e04925cb52554774a43a6e19e117a82aeb34740f
Lazy_coder <jainnancy779@gmail.com>
2023-09-25 20:26:22 +0530

    Update template.py

--- template.py
+++ template.py
@@ -2,20 +2,21 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: [%(message)s]:')
-project_name = "CNnClassifier"
-base_directory = ""  # Replace with your desired directory path
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "cnnClassifier"
 
 list_of_files = [
-    os.path.join(".github", "workflows", ".gitkeep"),
-    os.path.join("src", project_name, "__init__.py"),
-    os.path.join("src", project_name, "components", "__init__.py"),
-    os.path.join("src", project_name, "utils", "__init__.py"),
-    os.path.join("src", project_name, "config", "__init__.py"),
-    os.path.join("src", project_name, "config", "configuration.py"),
-    os.path.join("src", project_name, "pipeline", "__init__.py"),
-    os.path.join("src", project_name, "entity", "__init__.py"),
-    os.path.join("src", project_name, "constants", "__init__.py"),
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
@@ -23,22 +24,25 @@ list_of_files = [
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html"
+
+
 ]
 
+
 for filepath in list_of_files:
-    filepath = os.path.join(base_directory, filepath)  # Build the full path
+    filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir != "":
-        os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file: {filename}")
 
-    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
-        try:
-            with open(filepath, "w") as f:
-                pass
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
             logging.info(f"Creating empty file: {filepath}")
-        except PermissionError as e:
-            logging.error(f"Permission denied: {str(e)}")
+
+
     else:
-        logging.info(f"{filename} already exists")
+        logging.info(f"{filename} is already exists")
